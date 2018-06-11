fec4afe53a0e0f4bdb59f831c86d382a1dc12f47
jundai.hu <jundai.hu@jrdcom.com>
2018-06-11 15:00:10 +0800

    1.QR code.

---
+++ i18n_qrcodelink.js
@@ -0,0 +1,8 @@
+var i18n = {
+  en: {
+    notConnectedTip: "If not connecting to Google Play or APP Store automatically, please open it in your phone browser Chrome or Safari."
+  },
+  "zh-cn": {
+    notConnectedTip: "如果不能自动连接到Google Play或者APP Store，请使用Chrome或者Safari浏览器打开."
+  }
+}
